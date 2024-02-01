29bf6fe80ae9596ae3ae6ba6e5b2b9cdd9ac7a94
Amalrajtpnr <amalrajtpnr@gmail.com>
2024-02-01 14:43:21 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,243 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Chart as ChartJS, defaults } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import { useEffect, useState } from "react";
+import { Scatter } from "react-chartjs-2";
+import {
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  Chart,
+  PointElement,
+  LineElement,
+  DoughnutController,
+  ArcElement,
+  Legend,
+  Tooltip,
+} from "chart.js";
 
-const inter = Inter({ subsets: ["latin"] });
+// Register the required controllers and elements
+Chart.register(
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  DoughnutController,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  Legend,
+  Tooltip
+);
+
+// Register the required plugins
+Chart.register(CategoryScale, LinearScale);
 
 export default function Home() {
+  const [graph, setGraph] = useState<Array<{ x: string; y: number }>>();
+  const [pieChartData, setPieChartData] =
+    useState<Array<{ label: string; value: number }>>();
+  const [table, setTable] =
+    useState<
+      Array<{ id: number; name: string; quantity: number; price: number }>
+    >();
+
+  const fetchGraph = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/graph`);
+      if (response.ok) {
+        const data = await response.json();
+        setGraph(data);
+        console.log(data);
+      } else {
+        console.error("Error fetching graphs:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching graphs:", error);
+    }
+  };
+
+  const fetchPie = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/pie-chart`);
+      if (response.ok) {
+        const data = await response.json();
+        setPieChartData(data);
+        console.log(data);
+      } else {
+        console.error("Error fetching pie:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching pie:", error);
+    }
+  };
+
+  const fetchTable = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/table`);
+      if (response.ok) {
+        const data = await response.json();
+        setTable(data);
+        console.log(data);
+      } else {
+        console.error("Error fetching pie:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching pie:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGraph();
+    fetchPie();
+    fetchTable()
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen bg-black  flex flex-row items-center justify-center">
+      <div className="w-[20%] bg-[#20263a] h-full flex flex-col items-center justify-around">
+        <div className="w-full h-[20%] flex flex-col items-center justify-center">
+          <img
+            src="/Assets/Briefcase.png
+      "
+            alt=""
+          />
+          <img
+            src="/Assets/StatBoard.png
+      "
+            alt=""
+          />
+        </div>
+        <div className="w-full h-[50%] flex flex-col items-end justify-around">
+          <div className="w-[80%] h-[15%] bg-white flex flex-row items-center justify-center rounded-l-[10px]">
+            <img src="/Assets/Circled Menu.png" alt="" />
+            <button className="w-[40%] h-full bg-white rounded-l-[10px]">
+              Dashboard
+            </button>
+          </div>
+          <div className="w-[80%] h-[15%] bg-black flex flex-row items-center justify-center rounded-l-[10px]">
+            <img src="/Assets/Support.png" alt="" />
+            <button className="w-[40%] h-full text-white bg-black rounded-l-[10px]">
+              Support
+            </button>
+          </div>{" "}
+          <div className="w-[80%] h-[15%] bg-black flex flex-row items-center justify-center rounded-l-[10px]">
+            <img src="/Assets/Circled Menu.png" alt="" />
+            <button className="w-[40%] h-full text-white bg-black rounded-l-[10px]">
+              Plugins
+            </button>
+          </div>{" "}
+          <div className="w-[80%] h-[15%] bg-black flex flex-row items-center justify-center rounded-l-[10px]">
+            <img src="/Assets/Help.png" alt="" />
+            <button className="w-[40%] h-full text-white bg-black rounded-l-[10px]">
+              Help
+            </button>
+          </div>{" "}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-[80%] h-full flex flex-col items-center justify-start bg-[#e8edff]">
+        <div className="w-full h-[10%] flex flex-row items-center justify-between bg-[#e8edff]">
+          <h1>Good MOrning</h1>
+          <div className="w-[15%] h-full border-[2px]"></div>
+        </div>
+        <div className="w-full h-[45%] flex flex-row items-center justify-between p-4">
+          <div className="w-[45%] h-full ">
+            <Line
+              data={{
+                labels: graph ? graph.map((data) => data.x) : [],
+                datasets: [
+                  {
+                    label: "Values",
+                    data: graph ? graph.map((data) => data.y) : [],
+                    backgroundColor: "#064FF0",
+                    borderColor: "#064FF0",
+                  },
+                ],
+              }}
+              options={{
+                scales: {
+                  x: {
+                    type: "category",
+                  },
+                  y: {
+                    beginAtZero: true,
+                  },
+                },
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
+                },
+                plugins: {
+                  title: {
+                    text: "Custom Graph",
+                  },
+                },
+              }}
+            />
+          </div>
+          <div className="w-[45%] h-full ">
+            <Doughnut
+              data={{
+                labels: pieChartData?.map((data) => data.label) || [],
+                datasets: [
+                  {
+                    data: pieChartData?.map((data) => data.value),
+                    backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56",
+                      "#4CAF50",
+                      "#9966FF",
+                    ],
+                    hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56",
+                      "#4CAF50",
+                      "#9966FF",
+                    ],
+                  },
+                ],
+              }}
+              options={{
+                plugins: {
+                  title: {
+                    text: "Pie Chart",
+                    display: true,
+                  },
+                },
+              }}
+            />
+          </div>
+        </div>
+        <div className="w-full h-[45%] flex flex-row items-center justify-between p-4">
+          <div className="w-[45%] h-full border border-black">
+          <table className="border border-black w-full h-full">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table?.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
